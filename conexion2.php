f7dd55d3d11970bed26f940a9cb7f2e543519e9b
siberneitor <siberneitor@gmail.com>
2020-06-29 12:29:34 -0500

    se deja conexion mysql vieja y se agrega S a etiqueta agregar producto

--- conexion2.php
+++ conexion2.php
@@ -1,8 +1,12 @@
 <?php
-$server='localhost';
-$user='root';
-$password='root';
-$bd='sistemaventa';
+//$server='localhost';
+//$user='root';
+//$password='root';
+//$bd='sistemaventa';
+
+
+
+
 
 $conexion=mysql_connect($server,$user,$password) or die(mysql_error());
 $sdb=mysql_select_db($bd,$conexion) or die(mysql_error());
